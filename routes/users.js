22bb8d9d718930acfca2a3cf87b5826970b91c58
Chirapong01 <74916492+Chirapong01@users.noreply.github.com>
2024-07-06 20:40:54 +0700

    add updatePic and deletePic  endpoint

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const mongoose = require('mongoose')
 const User = require('../models/User.js')
 const bcrypt = require('bcryptjs')
+const fs = require('fs')
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => callback(null, './uploads'),
@@ -35,7 +36,7 @@ router.post('/login', async (req, res) => {
   }
 })
 
-router.post('/register', upload.single(), async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
     const email = await User.find({ userEmail: req.body.userEmail })
     if (email.length > 0) {
@@ -73,10 +74,66 @@ router.put('/update/:id', (req, res) => {
 })
 
 
-router.delete('/:id', (req, res) => {
+router.put('/updatePic/:id', upload.single('image'), async (req, res) => {
   try {
-    User.findByIdAndDelete(req.params.id).then((users) => {
+    console.log(req.file);
+    if (req.file.filename != '' || req.file.filename != null) {
+      req.body.image = req.file.filename
+    } else {
+      res.json(404, req.body)
+    }
+
+    const users = await User.findById(req.params.id)
+    if (users.image != req.file.filename) {
+      await fs.unlink('./uploads/' + users.image, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('remove img success');
+        }
+      })
+    }
+
+    await User.findByIdAndUpdate(req.params.id, req.body, { new: true }).then((user) => {
+      res.json(user)
+    })
+
+  } catch (err) {
+    console.error(err);
+  }
+})
+
+router.put('/deleteProfilePic/:id', async (req, res) => {
+  try {
+    req.body.image = ''
+    const users = await User.findById(req.params.id)
+    await User.findByIdAndUpdate(req.params.id, req.body, { new: true }).then((user) => {
+      res.json(user)
+    })
+    await fs.unlink('./uploads/' + users.image, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('remove img success');
+      }
+    })
+  } catch (err) {
+    console.error(err);
+  }
+})
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const users = await User.findById(req.params.id)
+    await User.findByIdAndDelete(req.params.id).exec().then((users) => {
       res.json(users);
+    })
+    await fs.unlink('./uploads/' + users.image, (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('remove success');
+      }
     })
   } catch (err) {
     console.error(err);
