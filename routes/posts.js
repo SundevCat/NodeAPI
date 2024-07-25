b80990750a5679e3db1c4fe47791a7807ddb6838
Chirapong Phomphoo <Chirapong@PLANTOYS.NET>
2024-07-25 18:01:03 +0700

    end point delete post

--- routes/posts.js
+++ routes/posts.js
@@ -6,30 +6,39 @@ const Posts = require('../models/Posts')
 const fs = require('fs')
 
 const storage = multer.diskStorage({
-    destination: (req, file, callback) => callback(null, './uploads'),
-    filename: (req, file, callback) => callback(null, file.originalname)
+  destination: (req, file, callback) => callback(null, './uploads'),
+  filename: (req, file, callback) => callback(null, file.originalname)
 })
 const upload = multer({ storage })
- 
+
 router.get('/', async (req, res) => {
-    try {
-      await Posts.find().then((post) => {
-        res.json(post)
-      })
-    } catch (err) {
-      console.error(err);
-    }
-  })
+  try {
+    await Posts.find().then((post) => {
+      res.json(post.reverse())
+    })
+  } catch (err) {
+    console.error(err);
+  }
+})
 
 router.post('/', async (req, res) => {
-    try {
-        console.log(req.body);
-        await Posts.create(req.body).then((post) => {
-            res.json(post)
-        });
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    await Posts.create(req.body).then((post) => {
+      res.json(post)
+    });
+  } catch (err) {
+    console.error(err);
+  }
+})
+
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    await Posts.findByIdAndDelete(req.params.id).then((post) => {
+      res.json(post)
+    })
+  } catch (err) {
+    console.error(err);
+  }
 })
 
 module.exports = router
